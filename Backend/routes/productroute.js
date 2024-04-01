43a9481f4e4e31aff09e51fbf648049e1fff326b
sonali21112001 <sonali21112001@gmail.com>
2024-04-01 21:04:57 +0530

    Add API Routes

--- Backend/routes/productroute.js
+++ Backend/routes/productroute.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { getAllProducts, createProduct } = require("../controllers/productcontroller");
+const { getAllProducts, createProduct, updateProduct, deleteProduct, getProductDetails } = require("../controllers/productcontroller");
 
 const router = express.Router();
 
 router.route("/products").get(getAllProducts);
 router.route("/product/new").post(createProduct);
+router.route("/product/:id").put(updateProduct).delete(deleteProduct).get(getProductDetails)
 
 
 module.exports = router
